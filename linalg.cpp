6c79ce466542a87d4543d482dded4e6ff8260d47
Yilu Chen <chenyilu2016@outlook.com>
2021-04-06 21:23:42 +0200

    add omp and add runmain.sh

--- linalg.cpp
+++ linalg.cpp
@@ -55,7 +55,7 @@ void cg_init(int nx)
 double hpc_dot(Field const& x, Field const& y, const int N)
 {
     double result = 0;
-    //#pragma omp parallel for schedule(static) shared(x, y) reduction (+: result)
+    #pragma omp parallel for schedule(static) shared(x, y) reduction (+: result)
     for (int i = 0; i < N; i++)
         result += x[i] * y[i];
 
@@ -68,7 +68,7 @@ double hpc_dot(Field const& x, Field const& y, const int N)
 double hpc_norm2(Field const& x, const int N)
 {
     double result = 0;
-    //#pragma omp parallel for schedule(static) shared(x) reduction (+: result)
+    #pragma omp parallel for schedule(static) shared(x) reduction (+: result)
     for (int i = 0; i < N; i++)
     {
         double xi = x[i];
@@ -83,7 +83,7 @@ double hpc_norm2(Field const& x, const int N)
 //static 
 void hpc_fill(Field& x, const double value, const int N)
 {
-    //#pragma omp parallel for schedule(static) shared (x, value)
+    #pragma omp parallel for schedule(static) shared (x, value)
     for (int i = 0; i < N; i++)
     {
         x[i]=value; 
@@ -100,7 +100,7 @@ void hpc_fill(Field& x, const double value, const int N)
 //
 void hpc_axpy(Field& y, const double alpha, Field const& x, const int N)
 {
-    //#pragma omp parallel for schedule(static) shared (x,y, alpha)
+    #pragma omp parallel for schedule(static) shared (x,y, alpha)
     for (int i = 0; i < N; i++)
     {
         y[i]=y[i]+alpha*x[i]; 
@@ -115,7 +115,7 @@ void hpc_axpy(Field& y, const double alpha, Field const& x, const int N)
 void hpc_add_scaled_diff(Field& y, Field const& x, const double alpha,
     Field const& l, Field const& r, const int N)
 {
-    //#pragma omp parallel for schedule(static) shared (x, y, alpha, l, r)
+    #pragma omp parallel for schedule(static) shared (x, y, alpha, l, r)
     for (int i = 0; i < N; i++)
     {
         y[i] = x[i] + alpha* (l[i]-r[i]); 
@@ -130,7 +130,7 @@ void hpc_add_scaled_diff(Field& y, Field const& x, const double alpha,
 void hpc_scaled_diff(Field& y, const double alpha,
     Field const& l, Field const& r, const int N)
 {
-    //#pragma omp parallel for schedule(static) shared(y, alpha, l, r)
+    #pragma omp parallel for schedule(static) shared(y, alpha, l, r)
     for (int i = 0; i < N; i++)
     {
         y[i] = alpha*(l[i]-r[i]); 
@@ -144,7 +144,7 @@ void hpc_scaled_diff(Field& y, const double alpha,
 //
 void hpc_scale(Field& y, const double alpha, Field& x, const int N)
 {
-    //#pragma omp parallel for schedule(static) shared(x, y, alpha) 
+    #pragma omp parallel for schedule(static) shared(x, y, alpha) 
     for (int i = 0; i < N; i++)
     {
         y[i] = alpha*x[i];
@@ -159,7 +159,7 @@ void hpc_scale(Field& y, const double alpha, Field& x, const int N)
 void hpc_lcomb(Field& y, const double alpha, Field& x, const double beta,
     Field const& z, const int N)
 {
-    //#pragma omp parallel for schedule(static) shared(x,y,z, alpha, beta)
+    #pragma omp parallel for schedule(static) shared(x,y,z, alpha, beta)
     for (int i = 0; i < N; i++)
     {
         y[i] = alpha*x[i]+beta*z[i]; 
@@ -172,7 +172,7 @@ void hpc_lcomb(Field& y, const double alpha, Field& x, const double beta,
 //
 void hpc_copy(Field& y, Field const& x, const int N)
 {
-    //#pragma omp parallel for schedule(static) shared (x,y)
+    #pragma omp parallel for schedule(static) shared (x,y)
     for (int i = 0; i < N; i++)
     {
         y[i] = x[i]; 
---
+++ runmain.sh
@@ -0,0 +1,29 @@
+export OMP_NUM_THREADS=1
+./main 128 100 0.005
+
+export OMP_NUM_THREADS=2
+./main 128 100 0.005
+
+export OMP_NUM_THREADS=4
+./main 128 100 0.005
+
+export OMP_NUM_THREADS=8
+./main 128 100 0.005
+
+export OMP_NUM_THREADS=16 
+./main 128 100 0.005 
+
+export OMP_NUM_THREADS=1
+./main 256 100 0.005
+
+export OMP_NUM_THREADS=2
+./main 256 100 0.005
+
+export OMP_NUM_THREADS=4
+./main 256 100 0.005
+
+export OMP_NUM_THREADS=8
+./main 256 100 0.005
+
+export OMP_NUM_THREADS=16 
+./main 256 100 0.005 
