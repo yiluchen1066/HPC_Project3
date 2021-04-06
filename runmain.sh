0fa0f976a743c138fb47cedd259a550d75e72e58
Yilu Chen <chenyilu2016@outlook.com>
2021-04-06 21:59:12 +0200

    fix 256 size

--- runmain.sh
+++ runmain.sh
@@ -14,16 +14,16 @@ export OMP_NUM_THREADS=16
 ./main 128 100 0.005 
 
 export OMP_NUM_THREADS=1
-./main 256 100 0.005
+./main 256 200 0.01
 
 export OMP_NUM_THREADS=2
-./main 256 100 0.005
+./main 256 200 0.01
 
 export OMP_NUM_THREADS=4
-./main 256 100 0.005
+./main 256 200 0.01
 
 export OMP_NUM_THREADS=8
-./main 256 100 0.005
+./main 256 200 0.01
 
 export OMP_NUM_THREADS=16 
-./main 256 100 0.005 
+./main 256 200 0.01 
