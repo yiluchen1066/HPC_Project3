2c90059c5ca1fdbe7812d188affb108d1b87bc47
Yilu Chen <chenyilu2016@outlook.com>
2021-04-06 22:03:07 +0200

    delete 256

--- runmain.sh
+++ runmain.sh
@@ -13,17 +13,3 @@ export OMP_NUM_THREADS=8
 export OMP_NUM_THREADS=16 
 ./main 128 100 0.005 
 
-export OMP_NUM_THREADS=1
-./main 256 200 0.01
-
-export OMP_NUM_THREADS=2
-./main 256 200 0.01
-
-export OMP_NUM_THREADS=4
-./main 256 200 0.01
-
-export OMP_NUM_THREADS=8
-./main 256 200 0.01
-
-export OMP_NUM_THREADS=16 
-./main 256 200 0.01 
