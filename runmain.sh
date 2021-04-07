ff817cc1ff5ae0a2808b5dccc43d1a0fd843b567
Yilu Chen <chenyilu2016@outlook.com>
2021-04-07 16:27:45 +0200

    add 256 runmain.sh

--- runmain.sh
+++ runmain.sh
@@ -1,15 +1,14 @@
 export OMP_NUM_THREADS=1
-./main 256 200 0.01
+./main 256 100 0.005
 
 export OMP_NUM_THREADS=2
-./main 256 200 0.01
+./main 256 100 0.005
 
 export OMP_NUM_THREADS=4
-./main 256 200 0.01
+./main 256 100 0.005
 
 export OMP_NUM_THREADS=8
-./main 256 200 0.01
+./main 256 100 0.005
 
 export OMP_NUM_THREADS=16 
-./main 256 200 0.01
-
+./main 256 100 0.005
