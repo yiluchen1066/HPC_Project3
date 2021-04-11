6561915d9f286f4ee94d3bf10d5c76eafb966123
Yilu Chen <chenyilu2016@outlook.com>
2021-04-11 23:45:20 +0200

    runmain.sh

--- runmain.sh
+++ runmain.sh
@@ -1,16 +1,98 @@
 export OMP_NUM_THREADS=1
 ./main 64 100 0.005
 
+export OMP_NUM_THREADS=2
+./main 64 100 0.005
+
 export OMP_NUM_THREADS=4
-./main  128 100 0.005
+./main 64 100 0.005
+
+export OMP_NUM_THREADS=8
+./main 64 100 0.005
+
+export OMP_NUM_THREADS=16
+./main 64 100 0.005
+
+export OMP_NUM_THREADS=24
+./main 64 100 0.005
+
+export OMP_NUM_THREADS=1
+./main 128 100 0.005
+
+export OMP_NUM_THREADS=2
+./main 128 100 0.005
+
+export OMP_NUM_THREADS=4
+./main 128 100 0.005
+
+export OMP_NUM_THREADS=8
+./main 128 100 0.005
+
+export OMP_NUM_THREADS=16
+./main 128 100 0.005
+
+export OMP_NUM_THREADS=24
+./main 128 100 0.005
+
+export OMP_NUM_THREADS=1
+./main 256 100 0.005
+
+export OMP_NUM_THREADS=2
+./main 256 100 0.005
+
+export OMP_NUM_THREADS=4
+./main 256 100 0.005
+
+export OMP_NUM_THREADS=8
+./main 256 100 0.005
 
 export OMP_NUM_THREADS=16
 ./main 256 100 0.005
 
-export OMP_NUM_THREADS=64
+export OMP_NUM_THREADS=24
+./main 256 100 0.005
+
+export OMP_NUM_THREADS=1
 ./main 512 100 0.005
 
-export OMP_NUM_THREADS=128
+export OMP_NUM_THREADS=2
+./main 512 100 0.005
+
+export OMP_NUM_THREADS=4
+./main 512 100 0.005
+
+export OMP_NUM_THREADS=8
+./main 512 100 0.005
+
+export OMP_NUM_THREADS=16
+./main 512 100 0.005
+
+export OMP_NUM_THREADS=24
+./main 512 100 0.005
+
+export OMP_NUM_THREADS=1
 ./main 1024 100 0.005
+
+export OMP_NUM_THREADS=2
+./main 1024 100 0.005
+
+export OMP_NUM_THREADS=4
+./main 1024 100 0.005
+
+export OMP_NUM_THREADS=8
+./main 1024 100 0.005
+
+export OMP_NUM_THREADS=16
+./main 1024 100 0.005
+
+export OMP_NUM_THREADS=24
+./main 1024 100 0.005
+
+lscpu 
+cat /proc/cpuinfo
+cat /proc/meminfo
+
+
+
 
 
